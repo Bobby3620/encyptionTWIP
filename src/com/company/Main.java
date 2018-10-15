cb21335b53a84d0f9817bf35f5568612793b959e
rd568 <bobbyday3620@gmail.com>
2018-10-15 11:19:22 -0400

    ayyyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Where objects exist
+        encrypt user1;
+
+        //Where the user is asked to input a word
+        Scanner kbInput = new Scanner(System.in);
+        System.out.println("Please input a word for encryption");
+        String userWord1 = kbInput.nextLine();
+        user1 = new encrypt(userWord1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //testing
+        String cool = "dlso";
+        char[] chars = cool.toCharArray();
+        System.out.println(chars);
+
+
+
     }
 }
---
+++ src/com/company/encrypt.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by rd568 on 10/15/18.
+ */
+public class encrypt {
+
+    //State vars
+    public String word;
+    public char[] wordToCharArray;
+
+    public encrypt(String w){
+        word = w;
+    }
+
+
+
+}
