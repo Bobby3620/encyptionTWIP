819b8af00c38518a2195ecc31397f842a6d5b26d
Bobby3620 <41356385+Bobby3620@users.noreply.github.com>
2018-10-18 19:30:16 -0400

    ayyyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,26 +14,37 @@ public class Main {
         String userWord1 = kbInput.nextLine();
         user1 = new encrypt(userWord1);
 
+        //Where the user chooses encryption key -> Caeser Cipher
+        int containKey;
+
+        System.out.println("Please enter any number between 1-26");
+        int key = kbInput.nextInt();
+
+            if (key <= 26) {
+                containKey = user1.encryptionKey(key);
+            }
+        System.out.println("Your encrypted word is ");
+        System.out.print(user1.setEncryptKey(userWord1));
+
+        //Where the user decides if they would like to decrypt it
+        Scanner kbInput2 = new Scanner(System.in);
+        System.out.println("Would you like to decrypt it?");
+        String yesOrno = kbInput2.nextLine();
+
+        if(yesOrno.equalsIgnoreCase("yes")){
+            System.out.println("What is the decryption key(same as encryption key)");
+            int decryptionKey = kbInput.nextInt();
+                if(decryptionKey == key){
+                    System.out.println("Your decrypted word is ");
+                    System.out.print(user1.setDecryptionKey(userWord1).toLowerCase());
+                }
+        }
+        else if(yesOrno.equalsIgnoreCase("no")){
+            System.out.println("Okay, goodbye!");
+        }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //testing
-        String cool = "dlso";
-        char[] chars = cool.toCharArray();
-        System.out.println(chars);
 
 
 
--- src/com/company/encrypt.java
+++ src/com/company/encrypt.java
@@ -7,11 +7,49 @@ public class encrypt {
 
     //State vars
     public String word;
-    public char[] wordToCharArray;
+    public int encryptKey;
+    public int greaterZ = (int)(Math.random() * 5);
 
-    public encrypt(String w){
-        word = w;
+    public encrypt(String kbInput){
+        word = kbInput;
     }
+
+    public int encryptionKey(int kbInput){
+        encryptKey = kbInput;
+        return encryptKey;
+    }
+
+    public String setEncryptKey(String kbInput){
+        String d = "";
+        String forReturn = "";
+        for(int index = 0; index < kbInput.length(); index++){
+            char caeserShift = (char)(kbInput.charAt(index) + encryptKey);
+            System.out.println(caeserShift);
+            if(caeserShift >= 'z'){
+                d = d + (char)(word.charAt(index) - (26 - greaterZ));
+                System.out.println(caeserShift + d);
+                System.out.println("I don't know where the random symbols are coming from if userWord1 is greater than z");
+                break;
+            }
+        }
+        return forReturn;
+    }
+
+    public String setDecryptionKey(String kbInput){
+        String forReturn = "";
+        String d = "";
+        for(int index = 0; index < kbInput.length(); index++){
+            char caeserShift = (char)(kbInput.charAt(index) - encryptKey);
+            System.out.println(caeserShift);
+            if(caeserShift >= 'z'){
+                d = d + (char)(word.charAt(index) - (26 - greaterZ));
+                System.out.println(caeserShift + d);
+            }
+        }
+        return forReturn;
+    }
+
+
 
 
 
